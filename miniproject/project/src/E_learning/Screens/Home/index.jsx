559d4fed63fc296ab5fe9f659a022f7b3deaa1f2
Kato030694 <longan.dbas@gmail.com>
2020-10-19 13:16:35 +0700

    Bat dau du an

---
+++ miniproject/project/src/E_learning/Screens/Home/index.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class HomeScreen extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="display-4" style={{color: "blue"}}>Danh Sach Khoa Hoc</h1>
+            </div>
+        )
+    }
+}
